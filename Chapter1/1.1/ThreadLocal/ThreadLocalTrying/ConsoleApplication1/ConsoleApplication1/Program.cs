7ff3f4e465c901d4af21d43d4c70039369e9aef5
Robertas <info@hrwproject.com>
2015-12-08 21:20:29 +0100

    Tests With ThreadLocal

--- Chapter1/1.1/ThreadLocal/ThreadLocalTrying/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ Chapter1/1.1/ThreadLocal/ThreadLocalTrying/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,30 +6,64 @@ namespace ConsoleApplication1
 {
     public static class Program
     {
-        public static ThreadLocal<int> _field = 
-            new ThreadLocal<int>(()=>
+        public static void Mytarget()
+        {
+            Console.WriteLine("target");  
+        }
+        public static void Mytarget1(string a)
+        {
+            Console.WriteLine("target1: {0}", a);
+        }
+
+        public static ThreadLocal<string> _field = 
+            new ThreadLocal<string>(()=>
             {
-                return Thread.CurrentThread.ManagedThreadId;
+                return Thread.CurrentThread.Name + "no-name";
             });
+
+        public static ThreadLocal<int> _field1 =
+         new ThreadLocal<int>(() =>
+         {
+             return Thread.CurrentThread.ManagedThreadId;
+         });
 
         static void Main(string[] args)
         {
+            //1.
+            ThreadStart ts = new ThreadStart(Mytarget);            
+            Thread t = new Thread(ts);
+            t.Start();
+
+
+            //2.
             new Thread(() =>
                 {
-                    for (int i = 0; i < _field.Value; i++)
+                    Mytarget1("2");
+                }).Start();
+
+            //3.            
+            new Thread( (a) => 
+            {
+
+                Console.WriteLine(""); 
+                Mytarget1("3");
+            }).Start();
+
+
+            new Thread(() =>
+                {
+                    for (int i = 0; i < 10; i++)
                     {
-                        Console.WriteLine("A: {0}", i);
-                        //Thread.Sleep(0);
+                        Console.WriteLine("A: {0}", _field.Value);                        
                     }
 
                 }).Start();
 
             new Thread(() =>
             {
-                for (int i = 0; i < _field.Value; i++)
+                for (int i = 0; i < _field1.Value; i++)
                 {
                     Console.WriteLine("B: {0}", i);
-                    //Thread.Sleep(0);
                 }
             }).Start();
 
