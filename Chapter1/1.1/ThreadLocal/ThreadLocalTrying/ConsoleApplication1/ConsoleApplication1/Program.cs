af7411a78379d35e82a2d4cae5a77e2c8f8b47ab
Robertas <info@hrwproject.com>
2015-12-08 20:08:34 +0100

    First Commit

---
+++ Chapter1/1.1/ThreadLocal/ThreadLocalTrying/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+
+namespace ConsoleApplication1
+{
+    public static class Program
+    {
+        public static ThreadLocal<int> _field = 
+            new ThreadLocal<int>(()=>
+            {
+                return Thread.CurrentThread.ManagedThreadId;
+            });
+
+        static void Main(string[] args)
+        {
+            new Thread(() =>
+                {
+                    for (int i = 0; i < _field.Value; i++)
+                    {
+                        Console.WriteLine("A: {0}", i);
+                        //Thread.Sleep(0);
+                    }
+
+                }).Start();
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < _field.Value; i++)
+                {
+                    Console.WriteLine("B: {0}", i);
+                    //Thread.Sleep(0);
+                }
+            }).Start();
+
+            Console.WriteLine("Press any key to stop");
+            Console.ReadKey();
+        }
+    }
+}
